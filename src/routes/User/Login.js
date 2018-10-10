92983132d34acd73b80fbd89c8ee31129b402181
wuweijian1997 <wuweijian1997@163.com>
2018-10-10 18:46:12 +0800

    添加请求 登陆

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -76,9 +76,10 @@ export default class LoginPage extends Component {
           <Submit loading={submitting}>登录</Submit>
           <div className={styles.other}>
             其他登录方式
-            <Icon className={styles.icon} type="alipay-circle" />
-            <Icon className={styles.icon} type="taobao-circle" />
-            <Icon className={styles.icon} type="weibo-circle" />
+            <a href="/qq_login/callback"> qq登陆</a>
+            <Link to="/qq_login/callback">
+              <Icon className={styles.icon} type="qq" theme="outlined" />
+            </Link>
             <Link className={styles.register} to="/user/register">
               注册账户
             </Link>
